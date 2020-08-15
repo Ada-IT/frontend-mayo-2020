2e171542cc0b68f961b3725b2981dca8f94ad726
Maximiliano José Sorribas <mjsorribas@gmail.com>
2020-08-15 13:01:04 -0300

    add ejemplos de clase js

---
+++ ejemplos/js/demo.js
@@ -0,0 +1,31 @@
+function alerta(data) {
+  alert(data);
+}
+
+function suma(a, b) {
+  resultado = a + b;
+  return resultado;
+}
+
+function capitalize(data) {
+  capitalized = data;
+  return capitalized;
+}
+
+// nombredefuncion(parametro)
+function cambiarColor(color) {
+  document.getElementById("demo").style.backgroundColor = color;
+}
+
+function cambiarWidth(width) {
+  document.getElementById("demo2").style.width = width;
+}
+
+function cambiarWidthFijo() {
+  miwith = "30%";
+  document.getElementById("demo2").style.width = miwith;
+}
+
+function cambiarTexto() {
+  document.getElementById("demo2").innerHTML = "Hola Marte!!!!";
+}
