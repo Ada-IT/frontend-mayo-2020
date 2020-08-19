bb6b5568e681753da001cc690c4d75dcb58f7694
Maximiliano José Sorribas <mjsorribas@gmail.com>
2020-08-18 21:59:48 -0300

    ultimos cambios js

--- ejemplos/js/demo.js
+++ ejemplos/js/demo.js
@@ -2,7 +2,7 @@ function alerta(data) {
   alert(data);
 }
 
-function suma(a, b) {
+function suma() {
   resultado = a + b;
   return resultado;
 }
@@ -14,18 +14,43 @@ function capitalize(data) {
 
 // nombredefuncion(parametro)
 function cambiarColor(color) {
-  document.getElementById("demo").style.backgroundColor = color;
+  var data = (document.getElementById("demo").style.backgroundColor = color);
 }
 
 function cambiarWidth(width) {
-  document.getElementById("demo2").style.width = width;
+  var data = (document.getElementById("demo2").style.width = width);
 }
 
 function cambiarWidthFijo() {
   miwith = "30%";
-  document.getElementById("demo2").style.width = miwith;
+  var data = (document.getElementById("demo2").style.width = miwith);
 }
 
-function cambiarTexto() {
-  document.getElementById("demo2").innerHTML = "Hola Marte!!!!";
+function cambiarTexto(elemento, mitexto) {
+  var data = (document.getElementById(elemento).innerHTML = mitexto);
+}
+
+function obtenerContenido(elemento) {
+  var data = document.getElementById(elemento).textContent;
+  alert(data);
+  return data;
+}
+
+function obtenerContenido2(elemento, mitexto) {
+  var data;
+  if (elemento == "demo2") {
+    data = document.getElementById(elemento).innerHTML = "hola mundo";
+  } else {
+    data = document.getElementById(elemento).innerHTML = mitexto;
+  }
+}
+
+function sumar(elemento) {
+  //con parseInt convierto a entero el string del valor
+  var n1 = parseInt(document.getElementById("a").value);
+  var n2 = parseInt(document.getElementById("b").value);
+  var total = n1 + n2; // sumo resultados.
+  console.log("mi suma es : " + total);
+  data = document.getElementById(elemento).innerHTML = "Resultado: " + total;
+  console.log(data);
 }
