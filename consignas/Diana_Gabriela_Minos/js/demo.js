9e8e45bb44b8b3deb0a54fac5fdc91dec7d50129
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2020-08-19 20:56:50 -0300

    agrego funciones y variables

--- consignas/Diana_Gabriela_Minos/js/demo.js
+++ consignas/Diana_Gabriela_Minos/js/demo.js
@@ -17,16 +17,46 @@ function cambiarColor(color) {
 }
 
 function cambiarWidth(width) {
-    document.getElementById("demo2").style.width = width;
+    document.getElementById('demo2').style.width = width;
 }
 
 function cambiarWidthFijo() {
     miwidth = "30%";
-    document.getElementById("demo2").style.width = miwidth;
+    document.getElementById('demo2').style.width = miwidth;
 
 }
 
 function cambiarTexto() {
-    document.getElementById("demo2").innerHTML = "Hola Mundo!!!";
+    document.getElementById('demo2').innerHTML = "Hola Mundo!!!";
+
 }
+
+function cambiarTexto(elemento, mitexto) {
+    document.getElementById(elemento).innerHTML = mitexto;
+}
+
+function obtenerContenido(elemento) {
+    var data = document.getElementById(elemento).textContent;
+    alert(data);
+    return data;
+}
+
+function obtenerContenido2(elemento, mitexto) {
+    if (elemento == 'demo2') {
+        document.getElementById(elemento).innerHTML = 'hola mundo';
+    } else {
+        document.getElementById(elemento).innerHTML = mitexto;
+    }
+}
+
+function sumar(elemento) {
+    //con parseInt convierto a entero el string del valor
+    var n1 = parseInt(document.getElementById('a').value);
+    var n2 = parseInt(document.getElementById('b').value);
+    var total = n1 + n2; //sumo resultados
+    console.log('mi suma es: ' + total);
+    data = document.getElementById(elemento).innerHTML = 'resultado:' + total;
+    console.log(data);
+}
+
 
