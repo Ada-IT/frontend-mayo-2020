1ae0139993398271f8010a96fc5f2727c0f4c4e2
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2020-08-21 22:44:14 -0300

    agrego funciones con while, for y arrays

--- consignas/Diana_Gabriela_Minos/js/demo.js
+++ consignas/Diana_Gabriela_Minos/js/demo.js
@@ -31,8 +31,8 @@ function cambiarTexto() {
 
 }
 
-function cambiarTexto(elemento, mitexto) {
-    document.getElementById(elemento).innerHTML = mitexto;
+function cambiarTexto2(elemento, mitexto) {
+    var data = (document.getElementById(elemento).innerHTML) = mitexto;
 }
 
 function obtenerContenido(elemento) {
@@ -55,8 +55,71 @@ function sumar(elemento) {
     var n2 = parseInt(document.getElementById('b').value);
     var total = n1 + n2; //sumo resultados
     console.log('mi suma es: ' + total);
-    data = document.getElementById(elemento).innerHTML = 'resultado:' + total;
+    data = document.getElementById(elemento).innerHTML = 'resultado: ' + total;
     console.log(data);
 }
 
+function obtenerContenido3(elemento, mitexto) {
+    var data;
+    if (elemento == 'demo2') {
+        data = document.getElementById(elemento).innerHTML = 'hola mundo';
+    } else {
+        data = document.getElementById(elemento).innerHTML = mitexto;
+    }
+}
+
+function mostrarNumeros() {
+    var n = 3;
+    while (n <= 5) {
+        console.log('contando: ' + n);
+        n++;
+    }
+    return n;
+}
+
+function llenarTabla() {
+    var n = 0; //contador
+    var celdas = ''; //variable para armar la tabla
+    while (n <= 5) {
+        console.log('contando: ' + n); //debug
+        celdas = celdas + '<tr><td>' + n + '</td></tr>';
+        console.log('celdas: ' + celdas); //debug
+        n++;
+
+    }
+    mitabla = cambiarTexto2('mitabla', celdas);
+    return n;
+}
+
+function llenarTabla2() {
+    var n = 0;
+    var celdas = '';
+    for (var i = 0; i <= 5; i++) {
+        console.log('contando: ' + i); //debug
+        celdas = celdas + '<tr><td>' + n + '</td></tr>';
+        console.log('celdas: ' + celdas); //debug
+    }
+    cambiarTexto2('mitabla', celdas);
+    return n;
+}
+
+function llenarTabla3() {
+    let frutas = ['Manzana', 'Banana', 'Pera', 'Kiwi', 'Naranja', 'Arandanos']; //array de datos
+    var celdas = '';
+    for (var i = 0; i <= 5; i++) {
+        console.log('contando: ' + i); //debug
+        celdas = celdas + '<tr><td>' + frutas[i] + '</td></tr>';
+        console.log('celdas: ' + celdas); //debug
+    }
+    cambiarTexto2('mitabla', celdas);
+    console.log('mi array: ' + frutas.length); //debug
+    console.log('mi array pos 3: ' + frutas[3]); //debug
+    return frutas;
+}
+
+function llenarTabla4() {
+    let frutas = ['Manzana', 'Banana', 'Pera', 'Kiwi', 'Naranja', 'Arandanos']; //array de datos
+    var celdas = '<tr><td>' + frutas[2] + '</td></tr>';
+    cambiarTexto('mitabla', celdas);
+}
 
