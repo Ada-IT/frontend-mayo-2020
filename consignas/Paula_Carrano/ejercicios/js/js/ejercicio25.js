d0e1bc7582f04d9ea014da2661b1436f0b9df988
Paula Carrano <paula.agustina.carrano@gmail.com>
2020-10-12 19:41:37 -0300

    updated 25

--- consignas/Paula_Carrano/ejercicios/js/js/ejercicio25.js
+++ consignas/Paula_Carrano/ejercicios/js/js/ejercicio25.js
@@ -1,55 +1,55 @@
-function getCategory(){
+function getCategory() {
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php", {
-        method:"get"
-    }) 
-    .then(response => response.json())
-    .then(
-        category => {
-            getInformation(category);
-        }
-    )
-    .catch(error => {
-        console.error(error);
+        method: "get"
     })
+        .then(response => response.json())
+        .then(
+            category => {
+                getInformation(category);
+            }
+        )
+        .catch(error => {
+            console.error(error);
+        })
 }
 
-function getInformation(category){
+function getInformation(category) {
     let categories = category.categories;
-    let rInformation =  [];
-    
+    let rInformation = [];
+
     categories.forEach(element => {
-       information = {
+        information = {
             "id": element.idCategory,
             "categoria": element.strCategory,
             "imagen": element.strCategoryThumb,
             "descripcion": element.strCategoryDescription,
         }
-        rInformation.push(information);   
+        rInformation.push(information);
     });
     orderByCategory(rInformation);
     fillCards(rInformation);
-    console.log(rInformation); 
+    console.log(rInformation);
 }
 
-function orderByCategory(rInformation){
+function orderByCategory(rInformation) {
     rInformation.sort(function (a, b) {
         if (a.categoria < b.categoria) {
-          return 1;
+            return 1;
         }
         if (a.categoria > b.categoria) {
-          return -1;
+            return -1;
         }
-        return 0; 
-      });     
+        return 0;
+    });
 }
 
-function fillCards(rInformation){
-    let cards ="";
+function fillCards(rInformation) {
+    let cards = "";
     rInformation.forEach(element => {
-    cards = cards +
-    `<div class="col-md-4">
+        cards = cards +
+            `<div class="col-md-4">
          <div class="card shadow border border-secondary p-3 m-3" style="width: 18rem;">
-              <img src="`+ element.imagen + `" class="card-img-top" alt="`+ element.categoria + `">
+              <img src="`+ element.imagen + `" class="card-img-top" alt="` + element.categoria + `">
                   <div class="card-body">
                   <h5 class="card-title text-center text-secondary font-weight-bolder">`+ element.categoria + `</h5>
                       <p class="card-text">`+ element.descripcion + `</p>
@@ -57,5 +57,5 @@ function fillCards(rInformation){
        </div>
     </div>`;
     });
-    document.getElementById("cardDetalle").innerHTML= cards;
+    document.getElementById("cardDetalle").innerHTML = cards;
 }
